465fb3a80b2569cb4ac9a137e922ec36d945f6a2
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-09-05 23:55:21 -0700

    add App spec

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,6 +2,15 @@ import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App', () => {
+  let component;
+  beforeEach(() => {
+    component = renderComponent('App')
+  })
+
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
+  })
+
   it('shows correct text', () => {
     const component = renderComponent(App);
     expect(component).to.contain('Redux Auth Boilerplate')
